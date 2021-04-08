8949bde7d6ed525226d2fa1b0a6128fee17749cb
bit1creative <hrytsenko.svan@gmail.com>
2021-04-08 12:21:59 +0300

    removed webpack from showing in browser

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
+    productionSourceMap: false,
+    publicPath:
+        process.env.NODE_ENV === 'production' ? '../server/public' : '/',
     outputDir: path.resolve(__dirname, '../server/public'),
     devServer: {
         proxy: {
